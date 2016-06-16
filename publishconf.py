e03193bb66c25941f91f3fc89b0e51c4ca019ca9
Alex Waite <Alexqw85@gmail.com>
2016-06-16 16:50:57 +0200

    Remove most templates; we don't need 'em

--- publishconf.py
+++ publishconf.py
@@ -9,15 +9,11 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://psychoinformatics.de'
-RELATIVE_URLS = False
-
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+#SITEURL = 'http://psychoinformatics.de'
+#RELATIVE_URLS = False
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
 #GOOGLE_ANALYTICS = ""
