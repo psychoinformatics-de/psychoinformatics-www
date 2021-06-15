e747930d4edcb551f125402b98c20b747db3c6d7
Alex Waite <alex@waite.eu>
2021-06-15 07:49:38 +0200

    simplify and uplift pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,32 +5,29 @@ from __future__ import unicode_literals
 #
 # About the site
 #
-AUTHOR = u'Michael Hanke & Alex Waite'
-SITETITLE = u'Psycho&shy;informatics'
-SITESUBTITLE = u'at <a href="http://www.fz-juelich.de/inm/inm-7/EN/Home/home_node.html">Jülich</a>'
-SITENAME = u'Psychoinformatics'
+AUTHOR = 'Michael Hanke & Alex Waite'
+SITETITLE = 'Psycho&shy;informatics'
+SITESUBTITLE = 'at <a href="http://www.fz-juelich.de/inm/inm-7/EN/Home/home_node.html">Jülich</a>'
+SITENAME = 'Psychoinformatics'
 SITEURL = ''
 
 TIMEZONE = 'Europe/Berlin'
-DEFAULT_LANG = u'en'
-LOCALE = u'en_US.UTF-8'
+DEFAULT_LANG = 'en'
+LOCALE = 'en_US.UTF-8'
 
 #
 # Configure Pelican a bit
 #
+PATH = 'content'
 PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = [ 'sitemap' ]
 SITEMAP = { 'format': 'xml' }
 
-DIRECT_TEMPLATES = ['404'] # unset all templates; add 404
+DIRECT_TEMPLATES = ['404']  # unset all templates; add 404
 THEME = 'theme'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
 
 #
 # Configure the site
